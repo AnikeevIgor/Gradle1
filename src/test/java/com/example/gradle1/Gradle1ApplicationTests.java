58dea4f4e3b10ce4bbd66361bfae201ae0247a03
Anikeev Igor <415b@mail.ru>
2022-10-12 04:19:15 +0300

    1

---
+++ src/test/java/com/example/gradle1/Gradle1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gradle1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gradle1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
